2339401614b06be56394e22f9fcffa85772547eb
Patrik Singer <singerpatrik@gmail.com>
2017-01-05 23:39:15 +0000

    keep score locally + add server side scripts to git + handle login session

--- app/src/main/java/com/example/laurabravopriegue/petiquiz/menu.java
+++ app/src/main/java/com/example/laurabravopriegue/petiquiz/menu.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import android.content.SharedPreferences;
+import android.widget.Button;
+
 public class menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        boolean loggedIn = pref.getBoolean("loggedIn", false);
+        if (loggedIn) {
+            Button loginButton = (Button) findViewById(R.id.button4);
+            loginButton.setText("LOG OUT");
+        }
     }
 
     public void sendMessage(View view)
@@ -21,8 +30,24 @@ public class menu extends AppCompatActivity {
 
     public void loginbutton(View view)
     {
-        Intent intent = new Intent(menu.this, Login.class);
-        startActivity(intent);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        boolean loggedIn = pref.getBoolean("loggedIn", false);
+        if (loggedIn) {
+            SharedPreferences.Editor editor = pref.edit();
+
+            editor.putBoolean("loggedIn", false);
+            editor.putString("user", "");
+            editor.putInt("score", 0);
+
+            editor.commit();
+
+            Button loginButton = (Button) findViewById(R.id.button4);
+            loginButton.setText("LOG IN");
+        }
+        else {
+            Intent intent = new Intent(menu.this, Login.class);
+            startActivity(intent);
+        }
     }
 
 

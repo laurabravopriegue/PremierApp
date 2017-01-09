6dec9092ca0b1cab894536a8bba2af4e7411e524
laurabravopriegue <laurabravopriegue@gmail.com>
2017-01-09 16:18:55 +0000

    Update Multiplayer.java

--- app/src/main/java/com/example/laurabravopriegue/petiquiz/Multiplayer.java
+++ app/src/main/java/com/example/laurabravopriegue/petiquiz/Multiplayer.java
@@ -330,7 +330,7 @@ public class Multiplayer extends Fragment {
             else {
                 str="http://mfcfund.ml/petiapp/GetQuestions.php";
             }*/
-            str="http://mfcfund.ml/petiapp/GetQuestions.php";
+            str="https://petiapp.000webhostapp.com/GetQuestions.php";
             URLConnection urlConn = null;
             BufferedReader bufferedReader = null;
             try
